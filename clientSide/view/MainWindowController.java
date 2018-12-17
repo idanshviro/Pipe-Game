13946e0e47e203942969dbd75bd3f71433e407c0
Idan Shviro <idanshviro@gmail.com>
2018-12-17 22:20:43 +0200

    Add counter of steps

--- clientSide/view/MainWindowController.java
+++ clientSide/view/MainWindowController.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.scene.control.TextField;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
@@ -7,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+
+import com.sun.javafx.collections.IntegerArraySyncer;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -22,7 +25,11 @@ public class MainWindowController implements Initializable{
 	private Board b;
 	private State<Board> boardState;
 	private SearchablePipeGame searchable;
+	private int steps;
 
+    @FXML
+    private TextField Steps;
+	
 	@FXML
 	PipeGameDisplayer pipeGameDisplayer;
 	public void openFile() {
@@ -35,8 +42,13 @@ public class MainWindowController implements Initializable{
 			loadLevel(chosen);
 		}
 	}
+	
+	public String getSteps() {
+	return String.valueOf(steps);
+	}
 
 	public void loadLevel(File f){
+		steps=0;
 		Scanner scanner = null;
 		List<char[]> level = new ArrayList<char[]>();
 		try {
@@ -60,9 +72,6 @@ public class MainWindowController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {	
-
-
-
 		pipeGameDisplayer.addEventFilter(MouseEvent.MOUSE_CLICKED, (e)->{pipeGameDisplayer.requestFocus();});
 		loadLevel(new File("./resources/Levels/1.txt"));
 
@@ -74,10 +83,15 @@ public class MainWindowController implements Initializable{
 				double h = pipeGameDisplayer.getH();
 				int x = (int) (event.getX()/w);
 				int y = (int) (event.getY()/h);
-				searchable.rotateOnBoard(y,x);
-				pipeGameDisplayer.redraw();				
-			}
+				char c = pipeGameDisplayer.pipeGameBoard.get(y)[x];
+				if(c == 'L' || c == 'l' ||c == 'F' || c == 'f' ||c == '7' || c == 'j'||c == 'J' || c == '-'||c == '|') {
+					steps++;
+					Steps.setText(getSteps());
+					searchable.rotateOnBoard(y,x);
+					pipeGameDisplayer.redraw();		
+				}
 
+			}
 		});
 	}
 }
