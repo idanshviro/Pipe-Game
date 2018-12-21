f3a55de8e321a5615894e8b635ddef5a0cabeb8d
בר קזז <barkazzaz@gmail.com>
2018-12-21 13:16:36 +0200

    December 21 2018
    MainWindowController:
    added none default constructor
    changed initializer to load from model
    
    Model: need to fix board eventListener in constructor

--- clientSide/view/Main.java
+++ clientSide/view/Main.java
@@ -1,7 +1,11 @@
 package view;
-	
+
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.PipeGameModel;
+import viewModel.PipeGameViewModel;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +15,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-			Scene scene = new Scene(root,400,400);
+			PipeGameModel m = new PipeGameModel();
+			PipeGameViewModel vm = new PipeGameViewModel(m);
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			MainWindowController mwc = new MainWindowController(vm);
+			fxmlLoader.setController(mwc);
+			BorderPane root = fxmlLoader
+					.load(getClass()
+							.getResource("MainWindow.fxml")
+							.openStream());
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -20,7 +32,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- serverSide/run/Main.java
+++ serverSide/run/Main.java
@@ -1,22 +1,7 @@
 package run;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import searchable.SearchablePipeGame;
-import state.Board;
-import state.State;
-
 public class Main {
 	public static void main(String[] args){
-		List<char[]> boardList = new ArrayList<char[]>();
-		boardList.add("s|g".toCharArray());
-		Board b = new Board(boardList);
-		State<Board> sb = new State<Board>(b);
-		System.out.println("before: " + sb.getState().getBoard().get(0)[1]);
-		
-		SearchablePipeGame spg = new SearchablePipeGame(sb);
-		spg.rotateOnBoard(0, 1);
-		System.out.println("after: " + sb.getState().getBoard().get(0)[1]);
+
 	}
 }
